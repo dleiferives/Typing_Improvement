d3678ce53fe3d87b04c2355dfdbb160b2121f041
dleiferives <dleiferives@gmail.com>
2025-01-09 14:42:21 -0800

    update to strings

--- main.c
+++ main.c
@@ -4,8 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 
-#define MAX_WORDS 100
-#define MAX_WORD_LEN 50
+#define MAX_INPUT_LEN 500
 
 // Log timing data to a CSV file
 void logTimingData(const char *filename, char key, long time_diff) {
@@ -19,7 +18,7 @@ void logTimingData(const char *filename, char key, long time_diff) {
 }
 
 // Run typing test
-void runTypingTest(const char *words[], int word_count, const char *filename) {
+void runTypingTest(const char *test_string, const char *filename) {
     initscr();
     noecho();
     cbreak();
@@ -28,58 +27,73 @@ void runTypingTest(const char *words[], int word_count, const char *filename) {
     init_pair(2, COLOR_RED, COLOR_BLACK);   // Red for incorrect
 
     struct timeval start, end;
+    int input_len = strlen(test_string);
+    char input[MAX_INPUT_LEN] = {0};
+    int current_pos = 0;
 
-    for (int i = 0; i < word_count; ++i) {
-        clear();
-        int len = strlen(words[i]);
-        char input[MAX_WORD_LEN] = {0};
-        int correct = 1;
+    clear();
+    mvprintw(0, 0, "%s", test_string); // Display test string
+    refresh();
 
-        mvprintw(0, 0, "%s", words[i]); // Display word
-        refresh();
+    gettimeofday(&start, NULL); // Start timing
 
-        gettimeofday(&start, NULL); // Start timing
+    while (1) {
+        char ch = getch();
 
-        for (int j = 0; j < len; ++j) {
-            char ch = getch();
+        if (ch == 127 || ch == 8) { // Handle backspace
+            if (current_pos > 0) {
+                current_pos--;
+                mvprintw(1, current_pos, " "); // Clear last character
+                refresh();
+            }
+        } else if (ch == 23) { // Handle Ctrl + Backspace (clear current word)
+            while (current_pos > 0 && input[current_pos - 1] != ' ') {
+                current_pos--;
+                mvprintw(1, current_pos, " ");
+            }
+            refresh();
+        } else if (ch == ' ' || ch == '\n') { // Handle space or Enter
+            if (ch == '\n' || current_pos == input_len) break; // End test
 
-            if (ch == words[i][j]) {
+            input[current_pos++] = ch;
+            mvprintw(1, current_pos - 1, "%c", ch);
+            refresh();
+        } else if (current_pos < input_len) { // Normal character input
+            input[current_pos++] = ch;
+            if (ch == test_string[current_pos - 1]) {
                 attron(COLOR_PAIR(1)); // Correct letters in green
             } else {
                 attron(COLOR_PAIR(2)); // Incorrect letters in red
-                correct = 0;
             }
-
-            mvprintw(0, j, "%c", ch); // Update word display with color
+            mvprintw(1, current_pos - 1, "%c", ch);
             attroff(COLOR_PAIR(1) | COLOR_PAIR(2));
             refresh();
-
-            input[j] = ch;
-
-            gettimeofday(&end, NULL); // End timing
-            long time_diff = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
-            logTimingData(filename, ch, time_diff);
-
-            gettimeofday(&start, NULL); // Reset start for next character
         }
 
-        input[len] = '\0';
-        if (strcmp(input, words[i]) == 0) {
-            mvprintw(1, 0, "Correct!\n");
-        } else {
-            mvprintw(1, 0, "Incorrect. Expected: %s\n", words[i]);
-        }
-        refresh();
-        getch(); // Wait for user input to proceed
+        gettimeofday(&end, NULL); // End timing for each key
+        long time_diff = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+        logTimingData(filename, ch, time_diff);
+
+        gettimeofday(&start, NULL); // Reset start for next character
     }
+
+    input[current_pos] = '\0'; // Null-terminate input
+
+    clear();
+    if (strcmp(input, test_string) == 0) {
+        mvprintw(0, 0, "Correct!\n");
+    } else {
+        mvprintw(0, 0, "Incorrect. Expected: %s\nYou typed: %s\n", test_string, input);
+    }
+    refresh();
+    getch();
 
     endwin();
 }
 
 int main() {
     const char *filename = "timing_data.csv";
-    const char *test_words[MAX_WORDS] = {"hello", "world", "test"};
-    int test_word_count = 3;
+    const char *test_string = "hello world test";
 
     FILE *file = fopen(filename, "w");
     if (file) {
@@ -87,7 +101,7 @@ int main() {
         fclose(file);
     }
 
-    runTypingTest(test_words, test_word_count, filename);
+    runTypingTest(test_string, filename);
 
     printf("Exiting program.\n");
     return 0;
